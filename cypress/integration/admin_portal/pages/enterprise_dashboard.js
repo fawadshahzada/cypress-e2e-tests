4bae429af674a1612290df77e09ddbf5ef73f618
Fawad Shahzada <fshahzada@synergyonline.com>
2022-03-03 22:30:22 +0500

    Spec file updated

--- cypress/integration/admin_portal/pages/enterprise_dashboard.js
+++ cypress/integration/admin_portal/pages/enterprise_dashboard.js
@@ -21,38 +21,38 @@ class EnterpriseDashboard {
     return cy.get(`:nth-child(${cardSequenceNumber})>.number-card .card-footer .d-flex .label`)
   }
 
-  getTableTitle() {
-    return cy.get('.table-title')
-  }
+  // getTableTitle() {
+  //   return cy.get('.table-title')
+  // }
 
-  getTableHeaders(tableName) {
-    return cy.get(`.${tableName} .table thead .btn-header>span`)
-  }
+  // getTableHeaders(tableName) {
+  //   return cy.get(`.${tableName} .table thead .btn-header>span`)
+  // }
 
-  getEmptyTableWarning() {
-    return cy.get('.alert-warning .message')
-  }
+  // getEmptyTableWarning() {
+  //   return cy.get('.alert-warning .message')
+  // }
 
-  getSpecificTableHeader(tableName, columnNumber) {
-    return cy.get(`.${tableName} .table thead th:nth-child(${columnNumber}) .btn-header>span`)
-  }
+  // getSpecificTableHeader(tableName, columnNumber) {
+  //   return cy.get(`.${tableName} .table thead th:nth-child(${columnNumber}) .btn-header>span`)
+  // }
 
-  openCardDetailedBreakdownArea(cardSequenceNumber) {
-    cy.get(`:nth-child(${cardSequenceNumber})>.number-card .footer-title>.toggle-collapse`)
-      .should('have.attr', 'aria-expanded', 'false')
-      .click()
-      .should('have.attr', 'aria-expanded', 'true')
-  }
+  // openCardDetailedBreakdownArea(cardSequenceNumber) {
+  //   cy.get(`:nth-child(${cardSequenceNumber})>.number-card .footer-title>.toggle-collapse`)
+  //     .should('have.attr', 'aria-expanded', 'false')
+  //     .click()
+  //     .should('have.attr', 'aria-expanded', 'true')
+  // }
 
-  clickCardDetailedBreakdownQuestion(cardSequenceNumber, questionText) {
-    return cy.get(`:nth-child(${cardSequenceNumber})>.number-card .card-footer .d-flex .label`)
-      .contains(questionText)
-      .click()
-  }
+  // clickCardDetailedBreakdownQuestion(cardSequenceNumber, questionText) {
+  //   return cy.get(`:nth-child(${cardSequenceNumber})>.number-card .card-footer .d-flex .label`)
+  //     .contains(questionText)
+  //     .click()
+  // }
 
-  resetTable() {
-    cy.get('.btn-sm.btn-outline-primary ').click()
-  }
+  // resetTable() {
+  //   cy.get('.btn-sm.btn-outline-primary ').click()
+  // }
 }
 
 export default EnterpriseDashboard
--- cypress/integration/admin_portal/pages/login_page.js
+++ cypress/integration/admin_portal/pages/login_page.js
@@ -1,7 +1,7 @@
 class LoginPage {
   loginUser(userEmail, userPassword) {
-    cy.get('#login-email').type(userEmail)
-    cy.get('#login-password').type(userPassword)
+    // cy.get('#login-email').type(userEmail)
+    // cy.get('#login-password').type(userPassword)
     cy.get('.action').should('have.text', 'Sign in').click()
   }
 }
